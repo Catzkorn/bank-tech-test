17d4d548b16a98cd3900a5fa3f3f4a8b5bc8954f
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-11-02 15:57:27 +0000

    Added feature test.
    Feature test checks for required acceptance criteria and passes.

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,6 +5,7 @@ describe Bank do
     @account = Bank.new
     @tenthjan = Time.new(2012, 1, 10)
     @thirteenthjan = Time.new(2012, 1, 13)
+    @fourteenthjan = Time.new(2012, 1, 14)
   end
 
   describe "#deposit" do
@@ -39,6 +40,15 @@ describe Bank do
 
     it "raises an error if a string is input as an amount" do
       expect { @account.withdraw("Goodbye!", @tenthjan).to raise_error("Inputted amount is not an integer") }
+    end
+  end
+
+  describe "Feature test" do
+    it "is given two deposits and provides one withdraw, and prints statement" do
+      @account.deposit(1000, @tenthjan)
+      @account.deposit(2000, @thirteenthjan)
+      @account.withdraw(500, @fourteenthjan)
+      expect(@account.statement).to eq("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00")
     end
   end
 end
